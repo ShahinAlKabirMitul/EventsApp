80d41cf5cadbda4092fb994fd540a126b6039994
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-06-13 22:14:42 +0600

    Add Event Dashboard Component

--- events_app/src/app/layout/App.jsx
+++ events_app/src/app/layout/App.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Events-App</h1>
-        <button className="ui icon button">
-          <i className="smile icon" />
-          CSS Button
-        </button>
-
-        <Button icon="smile" content="React Button" />
+        <EventDashboard />
       </div>
     );
   }
