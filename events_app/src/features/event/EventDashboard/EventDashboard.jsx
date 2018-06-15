22d3333a2aae4f8cf78764069277ba829787fc9b
ShahinAlKabirMitul <mitul.cse@gmail.com>
2018-06-15 14:28:19 +0600

    Submitting Data

--- events_app/src/features/event/EventDashboard/EventDashboard.jsx
+++ events_app/src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
+import cuid from 'cuid';
 const eventsDashboard = [
   {
     id: '1',
@@ -69,6 +70,16 @@ class EventDashboard extends Component {
       isOpen: false,
     });
   };
+  handleCreateEvent = newEvent => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    const updatedEvents = [...this.state.events, newEvent];
+    this.setState({
+      events: updatedEvents,
+      isOpen: false,
+    });
+    console.log(newEvent);
+  };
   render() {
     return (
       <Grid>
@@ -81,7 +92,12 @@ class EventDashboard extends Component {
             positive
             content="Create Event"
           />
-          {this.state.isOpen && <EventForm handleCancel={this.handleCancel} />}
+          {this.state.isOpen && (
+            <EventForm
+              createEvent={this.handleCreateEvent}
+              handleCancel={this.handleCancel}
+            />
+          )}
         </Grid.Column>
       </Grid>
     );
--- events_app/src/features/event/EventForm/EventForm.jsx
+++ events_app/src/features/event/EventForm/EventForm.jsx
@@ -6,13 +6,13 @@ class EventForm extends Component {
       title: '',
       date: '',
       city: '',
-      vanue: '',
+      venue: '',
       hostedBy: '',
     },
   };
   onFromSubmit = evt => {
     evt.preventDefault();
-    console.log(this.state.event);
+    this.props.createEvent(this.state.event);
   };
   onInputChange = evt => {
     const newEvent = this.state.event;
@@ -58,9 +58,9 @@ class EventForm extends Component {
           <Form.Field>
             <label>Venue</label>
             <input
-              name="vanue"
+              name="venue"
               onChange={this.onInputChange}
-              value={event.vanue}
+              value={event.venue}
               placeholder="Enter the Venue of the event"
             />
           </Form.Field>
