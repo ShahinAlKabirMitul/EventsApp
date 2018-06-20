3ec26d5069ef50d432d7ceb978e51b4733d3ce52
shahinalkabirmitul <mitul.cse@gmail.com>
2018-06-21 00:34:22 +0600

    Home page added

--- events_app/src/app/layout/App.jsx
+++ events_app/src/app/layout/App.jsx
@@ -1,31 +1,40 @@
-import React, { Component } from 'react';
-import {Route} from 'react-router-dom'
-import { Container } from 'semantic-ui-react';
-import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar/NavBar';
-import HomePage from '../../features/home/HomePage';
-import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
-import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
-import SettingsDashboard from '../../features/user/Settings/SettingsDashboard'
-import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage'
-import EventForm from '../../features/event/EventForm/EventForm';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
+import HomePage from "../../features/home/HomePage";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import EventForm from "../../features/event/EventForm/EventForm";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-           <Route exact path='/' component={HomePage}/>
-           <Route path='/events' component={EventDashboard}/>
-           <Route path='/event/:id' component={EventDetailedPage}/>
-           <Route path='/people' component={PeopleDashboard}/>
-           <Route path='/profile/:id' component={UserDetailedPage}/>
-           <Route path='/settings' component={SettingsDashboard}/>        
-           <Route path='/createevent' component={EventForm}/>
-
-         
-        </Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createevent" component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
   }
--- events_app/src/features/nav/NavBar/NavBar.jsx
+++ events_app/src/features/nav/NavBar/NavBar.jsx
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
-import { Menu, Container, Button } from 'semantic-ui-react';
-import {NavLink,Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link } from "react-router-dom";
 class NabBar extends Component {
   render() {
     return (
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header as={Link} to='/'>
+          <Menu.Item header as={Link} to="/">
             <img src="assets/logo.png" alt="logo" />
             Re-vents
           </Menu.Item>
-          <Menu.Item as={NavLink} to='/events' name="Events" />
-          <Menu.Item as={NavLink} to='/people' name="People" />
+          <Menu.Item as={NavLink} to="/events" name="Events" />
+          <Menu.Item as={NavLink} to="/people" name="People" />
           <Menu.Item>
-            <Button floated="right" as={Link} to='/createevent' positive inverted content="Create Event" />
+            <Button
+              floated="right"
+              as={Link}
+              to="/createevent"
+              positive
+              inverted
+              content="Create Event"
+            />
           </Menu.Item>
           <Menu.Item position="right">
             <Button basic inverted content="Login" />
@@ -21,7 +28,7 @@ class NabBar extends Component {
               basic
               inverted
               content="Sign Out"
-              style={{ marginLeft: '0.5em' }}
+              style={{ marginLeft: "0.5em" }}
             />
           </Menu.Item>
         </Container>
