96e46a97e8c1be175b8d208a05db176553d515c4
shahinalkabirmitul <mitul.cse@gmail.com>
2018-06-30 10:34:08 +0600

    Setting up validation for the event form

--- events_app/src/features/event/EventForm/EventForm.jsx
+++ events_app/src/features/event/EventForm/EventForm.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Segment, Form, Button,Grid,Header } from 'semantic-ui-react';
 import {connect} from 'react-redux';
 import {reduxForm,Field} from 'redux-form';
+import {composeValidators,combineValidators,isRequired,hasLengthGreaterThan} from 'revalidate';
 import cuid from 'cuid';
 import {createEvent,updateEvent} from '../eventActions'
 import TextInput from '../../../app/common/form/TextInput';
@@ -29,6 +30,18 @@ const category = [
     {key: 'music', text: 'Music', value: 'music'},
     {key: 'travel', text: 'Travel', value: 'travel'},
 ];
+const validate = combineValidators({
+  title : isRequired({message: 'The event title is required'}),
+  category : isRequired({message: 'Please provide a category'}),
+  description:composeValidators(
+    isRequired({message:'Please enter a description '}),
+
+    hasLengthGreaterThan(4)({message:'Description needs to be at lest 5 characters'})
+  )(),
+  city: isRequired('city'),
+  venue: isRequired('venue')
+
+})
 class EventForm extends Component {
 
 
@@ -51,6 +64,7 @@ class EventForm extends Component {
   };
  
   render() {
+    const {invalid, submitting, pristine} = this.props;
     return (
       <Grid>
         <Grid.Column width={10}>
@@ -66,10 +80,10 @@ class EventForm extends Component {
                 <Field name='venue' type='text' component={TextInput}  placeholder='Event Venue'/>
                 <Field name='date' type='text' component={TextInput}  placeholder='Event Date'/>
             
-                <Button positive type="submit">
+                <Button disabled={invalid || submitting || pristine} positive type="submit">
                   Submit
                 </Button>
-                <Button onClick={this.props.history.goBack} type="button">
+                <Button  onClick={this.props.history.goBack} type="button">
                   Cancel
                 </Button>
               </Form>
@@ -80,4 +94,4 @@ class EventForm extends Component {
     );
   }
 }
-export default connect(mapState,actions) (reduxForm({form : 'EventForm',enableReinitialize:true})(EventForm));
+export default connect(mapState,actions) (reduxForm({form : 'EventForm',enableReinitialize:true,validate})(EventForm));
