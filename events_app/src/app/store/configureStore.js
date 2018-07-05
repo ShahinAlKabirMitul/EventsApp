f425fd23c6223fd42403228a82d8e45b4d7fe4f8
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-05 07:56:24 +0600

    Redux thunk  starting

--- events_app/src/app/store/configureStore.js
+++ events_app/src/app/store/configureStore.js
@@ -1,29 +1,20 @@
-import {
-    createStore,
-    applyMiddleware
-} from 'redux';
-import {
-    composeWithDevTools
-} from 'redux-devtools-extension';
-import rootReducer from '../reducers/rootReducer'
-export const configureStore = (preloadedState) => {
-    const middlewares = [];
-    const middlewaresEnhancer = applyMiddleware(...middlewares);
-    const storeEnhancers = [middlewaresEnhancer];
-    const composeEnhancer = composeWithDevTools(...storeEnhancers)
-    const store = createStore(
-        rootReducer,
-        preloadedState,
-        composeEnhancer
-    )
-    if (process.env.NODE_ENV !== 'production') {
-        if (module.hot) {
-            module.hot.accept('../reducers/rootReducer.js', () => {
-                const newRootReducer = require('../reducers/rootReducer').default;
-                store.replaceReducer(newRootReducer);
-            })
-
-        }
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from '../reducers/rootReducer';
+import thunk from 'redux-thunk'
+export const configureStore = preloadedState => {
+  const middlewares = [thunk];
+  const middlewaresEnhancer = applyMiddleware(...middlewares);
+  const storeEnhancers = [middlewaresEnhancer];
+  const composeEnhancer = composeWithDevTools(...storeEnhancers);
+  const store = createStore(rootReducer, preloadedState, composeEnhancer);
+  if (process.env.NODE_ENV !== 'production') {
+    if (module.hot) {
+      module.hot.accept('../reducers/rootReducer.js', () => {
+        const newRootReducer = require('../reducers/rootReducer').default;
+        store.replaceReducer(newRootReducer);
+      });
     }
-    return store;
-}
+  }
+  return store;
+};
