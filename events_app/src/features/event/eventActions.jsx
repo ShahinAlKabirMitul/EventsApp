e351dae5e43da420d2264503ceb3bccd4b867091
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-05 23:39:42 +0600

    Fetching events from the mock API

--- events_app/src/features/event/eventActions.jsx
+++ events_app/src/features/event/eventActions.jsx
@@ -1,31 +1,60 @@
 import {
   CREATE_EVENT,
   UPDATE_EVENT,
-  DELETE_EVENT
+  DELETE_EVENT,
+  FETCH_EVENT,
 } from './eventConstants';
 
-export const createEvent = (event) => {
+import {
+  asyncActionStart,
+  asyncActionFinish,
+  asyncActionError,
+} from '../async/asyncActions';
+import { fetchSampleData } from '../../app/common/data/mockApi';
+import { AssertionError } from 'assert';
+
+export const fetchEvent = events => {
+  return {
+    type: FETCH_EVENT,
+    payload: events,
+  };
+};
+export const createEvent = event => {
   return {
     type: CREATE_EVENT,
     payload: {
-      event
-    }
-  }
-}
+      event,
+    },
+  };
+};
 
-export const updateEvent = (event) => {
+export const updateEvent = event => {
   return {
     type: UPDATE_EVENT,
     payload: {
-      event
-    }
-  }
-}
-export const deleteEvent = (eventId) => {
+      event,
+    },
+  };
+};
+export const deleteEvent = eventId => {
   return {
     type: DELETE_EVENT,
     payload: {
-      eventId
+      eventId,
+    },
+  };
+};
+
+export const loadEvent = () => {
+  return async dispatch => {
+    try {
+      dispatch(asyncActionStart());
+      let events = await fetchSampleData();
+      dispatch(fetchEvent(events));
+      dispatch(asyncActionFinish());
+    } catch (error) {
+      console.log(error);
+      dispatch(asyncActionError());
     }
-  }
-}
+  };
+};
--- events_app/src/features/event/eventReducer.jsx
+++ events_app/src/features/event/eventReducer.jsx
@@ -1,5 +1,10 @@
 import { createReducer } from '../../app/common/util/reducerUtil';
-import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT } from './eventConstants';
+import {
+  CREATE_EVENT,
+  UPDATE_EVENT,
+  DELETE_EVENT,
+  FETCH_EVENT,
+} from './eventConstants';
 
 const initialState = [];
 
@@ -17,8 +22,14 @@ export const updateEvent = (state, payload) => {
 export const deleteEvent = (state, payload) => {
   return [...state.filter(event => event.id !== payload.eventId)];
 };
+
+export const fetchEvents = (state, payload) => {
+  return payload.events;
+};
+
 export default createReducer(initialState, {
   [CREATE_EVENT]: createEvent,
   [UPDATE_EVENT]: updateEvent,
   [DELETE_EVENT]: deleteEvent,
+  [FETCH_EVENT]: fetchEvents,
 });
