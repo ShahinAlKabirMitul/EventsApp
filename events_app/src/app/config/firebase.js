1851e5e482e99fca420704d52ba3b33be459b62f
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-07 10:11:54 +0600

    Firestore 4.13.0 update to date objects

--- events_app/src/app/config/firebase.js
+++ events_app/src/app/config/firebase.js
@@ -9,5 +9,9 @@ const firebaseConfig = {
   messagingSenderId: '101731016219',
 };
 firebase.initializeApp(firebaseConfig);
-firebase.firestore();
+const firestore = firebase.firestore();
+const settings = {
+  timestampsInSnapshots: true,
+};
+firestore.settings(settings);
 export default firebase;
