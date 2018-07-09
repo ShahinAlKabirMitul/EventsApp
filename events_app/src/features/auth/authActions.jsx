21fc71ced1e1f422a0ab0150325959ca015bce21
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-09 07:05:30 +0600

    Error handing in the Register form

--- events_app/src/features/auth/authActions.jsx
+++ events_app/src/features/auth/authActions.jsx
@@ -42,5 +42,8 @@ export const registerUser = user => async (
     dispatch(closeModal());
   } catch (error) {
     console.log(error);
+    throw new SubmissionError({
+      _error: error.message,
+    });
   }
 };
