1c911c23ef24198bfc5419eb95b13a3c5bb3fb48
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-09 07:21:25 +0600

    Social login component

--- events_app/src/features/auth/Login/LoginForm.jsx
+++ events_app/src/features/auth/Login/LoginForm.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Form, Segment, Button, Label } from 'semantic-ui-react';
+import { Form, Segment, Button, Label, Divider } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import TextInput from '../../../app/common/form/TextInput';
 import { login } from '../authActions';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const actions = {
   login,
@@ -24,10 +25,17 @@ const LoginForm = ({ login, handleSubmit, error }) => {
           type="password"
           placeholder="password"
         />
-        {error && <Label  basic color='red'> {error} </Label>}
+        {error && (
+          <Label basic color="red">
+            {' '}
+            {error}{' '}
+          </Label>
+        )}
         <Button fluid size="large" color="teal">
           Login
         </Button>
+        <Divider horizontal>Or</Divider>
+        <SocialLogin />
       </Segment>
     </Form>
   );
